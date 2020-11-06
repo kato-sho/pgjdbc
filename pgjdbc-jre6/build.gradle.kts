fd19a3da3ffac9b0202eb62aa8113517d6fe0f8b
kato-sho <kato-sho@fujitsu.com>
2020-11-06 17:45:00 +0900

    fix: pgjdbc-jre6 build failure.

--- pgjdbc-jre6/build.gradle.kts
+++ pgjdbc-jre6/build.gradle.kts
@@ -63,11 +63,13 @@ dependencies {
 }
 
 val preprocessMain by tasks.registering(JavaCommentPreprocessorTask::class) {
+    dependsOn(sourceWithoutCheckerAnnotations)
     baseDir.set(layout.file(provider { sourceWithoutCheckerAnnotations.singleFile }))
     sourceFolders.addAll("src/main/java", "src/main/version")
 }
 
 val preprocessTest by tasks.registering(JavaCommentPreprocessorTask::class) {
+    dependsOn(sourceWithoutCheckerAnnotations)
     baseDir.set(layout.file(provider { sourceWithoutCheckerAnnotations.singleFile }))
     sourceFolders.add("src/test/java")
 }
